482995660d4b0d42a6bd1cd435ea78b02f6ceca0
Arnav Kohli <arnavkohli2003@gmail.com>
2023-06-08 15:52:32 +0100

    added cv stuff

---
+++ util/cv.js
@@ -0,0 +1,14 @@
+class Line{
+    constructor(x, y, l){
+        this.startx = x;
+        this.starty = y;
+        this.endx = x+l;
+        this.endy = y;
+    }
+
+    updateLengths(x, y, l){
+        
+    }
+};
+
+module.exports = Line;
