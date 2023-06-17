74bdaf840ca5c44d02af23c56620c53cd937a6ba
Arnav Kohli <arnavkohli2003@gmail.com>
2023-06-17 23:21:08 +0100

    tested reverse options

--- util/graph.js
+++ util/graph.js
@@ -11,17 +11,17 @@ class Graph {
         }
     }
 
-    addEdge(edgelist, weight){ // loops in the graph are okay, Dijkstra's should be able to deal with it
+    addEdge(edgelist, weight, option){ // loops in the graph are okay, Dijkstra's should be able to deal with it
         if(this.graph.hasOwnProperty(edgelist[0])){
             // the parent node already exists and doesnt have to be created
             // edgelist is the value to "vertices" in the edges part of the response to datadump
             // weight is the value for the weight key
-            this.graph[edgelist[0]][edgelist[1]] = weight;
+            this.graph[edgelist[0]][edgelist[1]] = [weight, option];
         } else {
             // the parent node doesnt exist and needs to be assigned
-            this.graph[edgelist[0]] = { };
+            this.graph[edgelist[0]] = {  };
 
-            this.graph[edgelist[0]][edgelist[1]] = weight;
+            this.graph[edgelist[0]][edgelist[1]] = [weight, option];
         }
     }
 
@@ -36,8 +36,8 @@ class Graph {
 
         for(const parent in this.graph){
             for(const child in this.graph[parent]){
-                if(this.graph[parent][child] + this.shortestTree[parent].distance <= this.shortestTree[child].distance){
-                    this.shortestTree[child].distance = this.graph[parent][child] + this.shortestTree[parent].distance;
+                if(this.graph[parent][child][0] + this.shortestTree[parent].distance <= this.shortestTree[child].distance){
+                    this.shortestTree[child].distance = this.graph[parent][child][0] + this.shortestTree[parent].distance;
                     this.shortestTree[child].previous = parent;
                 }
             }
@@ -54,8 +54,8 @@ class Graph {
         }
 
         for(const child in this.graph['E']){
-            if(this.graph['E'][child] + this.shortestTree['E'].distance <= this.shortestTree[child].distance){
-                this.shortestTree[child].distance = this.graph['E'][child] + this.shortestTree['E'].distance;
+            if(this.graph['E'][child][0] + this.shortestTree['E'].distance <= this.shortestTree[child].distance){
+                this.shortestTree[child].distance = this.graph['E'][child][0] + this.shortestTree['E'].distance;
                 this.shortestTree[child].previous = 'E';
             }
         }
@@ -63,8 +63,8 @@ class Graph {
         for(const parent in this.graph){
             if(parent !== 'E'){
                 for(const child in this.graph[parent]){
-                    if(this.graph[parent][child] + this.shortestTree[parent].distance <= this.shortestTree[child].distance){
-                        this.shortestTree[child].distance = this.graph[parent][child] + this.shortestTree[parent].distance;
+                    if(this.graph[parent][child][0] + this.shortestTree[parent].distance <= this.shortestTree[child].distance){
+                        this.shortestTree[child].distance = this.graph[parent][child][0] + this.shortestTree[parent].distance;
                         this.shortestTree[child].previous = parent;
                     }
                 }
@@ -83,6 +83,13 @@ class Graph {
 
         return returnList.reverse();
     }
+
+    nextoption(unexploredNode, currentNode){
+        this.Backtrack(currentNode);
+        var optionsList = this.reconstruct(unexploredNode, currentNode);
+        //console.log(optionsList[1]);
+        console.log(this.graph[optionsList[0]][optionsList[1]][1]);
+    }
 };
 
 var graph = new Graph(); 
@@ -93,21 +100,21 @@ graph.addVertex('C');
 graph.addVertex('D');
 graph.addVertex('E');
 
-graph.addEdge(['A', 'B'], 4); // this format is useful in the server file
-graph.addEdge(['B', 'A'], 4);
-graph.addEdge(['A', 'C'], 2);
-graph.addEdge(['C', 'A'], 2);
-graph.addEdge(['B', 'C'], 1);
-graph.addEdge(['C', 'B'], 1);
-graph.addEdge(['B', 'D'], 1);
-graph.addEdge(['D', 'B'], 1);
-graph.addEdge(['C', 'D'], 8);
-graph.addEdge(['D', 'C'], 8);
-graph.addEdge(['C', 'E'], 10);
-graph.addEdge(['E', 'C'], 10);
+graph.addEdge(['A', 'B'], 4, 90); // this format is useful in the server file
+graph.addEdge(['B', 'A'], 4, 180);
+graph.addEdge(['A', 'C'], 2, 200);
+graph.addEdge(['C', 'A'], 2, 220);
+graph.addEdge(['B', 'C'], 1, 240);
+graph.addEdge(['C', 'B'], 1, 260);
+graph.addEdge(['B', 'D'], 1, 80);
+graph.addEdge(['D', 'B'], 1, 70);
+graph.addEdge(['C', 'D'], 8, 50);
+graph.addEdge(['D', 'C'], 8, 280);
+graph.addEdge(['C', 'E'], 10, 300);
+graph.addEdge(['E', 'C'], 10, 320);
 
-graph.Backtrack('E');
-console.log(graph);
+graph.nextoption('C', 'E');
+//console.log(graph);
 // graph.Dijkstra();
 // console.log(graph);
 //console.log(graph.reconstruct('C', 'E'));
